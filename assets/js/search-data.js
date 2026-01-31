ebf73413a72020bfbdd5d750d8a3adc0e1fab08b
tommasomncttn <91601166+tommasomncttn@users.noreply.github.com>
2026-01-31 00:33:04 +0000

    Deploying to gh-pages from @ tommasomncttn/tommasomncttn.github.io@30ae8df6e7bb8ab37a22d0baa2aba07e2f88540f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "post-untitled-post",
+        
+          title: "Untitled Post",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/untitled-post/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
